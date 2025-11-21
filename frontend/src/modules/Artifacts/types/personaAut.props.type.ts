d2053337ea040680dc7b0083f93ba229f1a8c76b
Guilherme Moraes <guighm@outlook.com.br>
2025-11-20 22:03:24 -0400

    fix: resolve merge conflict

--- frontend/src/modules/Artifacts/types/personaAut.props.type.ts
+++
@@ -1,7 +0,0 @@
-import { Control } from "react-hook-form";
-import { InputsForm } from "../hooks/usePersonaAutForm";
-
-export type PersonaAutStepProps = {
-    language: string;
-    control: Control<InputsForm, any, InputsForm>;
-}
