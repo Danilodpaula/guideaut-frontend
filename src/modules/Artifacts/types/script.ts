1bf534e573c94a9d298e7c8f320096f8a3ce8bbc
Guilherme Moraes <guighm@outlook.com.br>
2025-11-26 14:34:32 -0400

    Alteração de Componentes

--- src/modules/Artifacts/types/script.ts
+++
@@ -1,5 +0,0 @@
-type ViewType = "script-select" | "create-script" | "update-script" | "";
-
-type FormType = "client" | "caregiver" | "therapist" | "custom" | "";
-
-export type { ViewType, FormType };
